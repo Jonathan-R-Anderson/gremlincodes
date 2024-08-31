c75006366710c41d30c95bb77ad631d22bdc6fbd
Jonathan Anderson <jranderson404@gmail.com>
2024-08-31 15:22:31 -0400

    fixing seeding bug

--- app.py
+++ app.py
@@ -147,9 +147,5 @@ def serve_file(filename):
         return send_file(file_path)
     return "File not found", 404
 
-@app.route('/')
-def index():
-    return render_template('index.html', gremlinThreadABI=json.dumps(gremlinThreadABI), gremlinThreadAddress=gremlinThreadAddress)
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- blueprints/routes.py
+++ blueprints/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
-
+from shared import gremlinThreadABI, gremlinThreadAddress
 blueprint = Blueprint('blueprint', __name__)
 
 @blueprint.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', gremlinThreadABI=json.dumps(gremlinThreadABI), gremlinThreadAddress=gremlinThreadAddress)
