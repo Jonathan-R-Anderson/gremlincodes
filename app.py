c70167546218c57499c653ac1332d6ac11116fd5
Jonathan Anderson <jranderson404@gmail.com>
2024-08-31 15:19:21 -0400

    fixing seeding bug

--- app.py
+++ app.py
@@ -148,8 +148,7 @@ def serve_file(filename):
 
 @app.route('/')
 def index():
-    return render_template('index.html', gremlinThreadABI=gremlinThreadABI if gremlinThreadABI else [],
-        gremlinThreadAddress=gremlinThreadAddress)
+    return render_template('index.html', gremlinThreadABI=gremlinThreadABI, gremlinThreadAddress=gremlinThreadAddress)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
