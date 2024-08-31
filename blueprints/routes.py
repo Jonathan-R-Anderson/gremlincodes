ec4c74759e4038a89bca720d70a3df162e0d8660
Jonathan Anderson <jranderson404@gmail.com>
2024-08-31 15:37:13 -0400

    fixing seeding bug

--- blueprints/routes.py
+++ blueprints/routes.py
@@ -7,5 +7,5 @@ blueprint = Blueprint('blueprint', __name__)
 @blueprint.route('/')
 def index():
     return render_template('index.html', 
-                           gremlinThreadABI=json.dumps(gremlinThreadABI[0], ensure_ascii=False),  # Avoid ASCII escaping
+                           gremlinThreadABI=json.dumps(gremlinThreadABI, ensure_ascii=False),  # Avoid ASCII escaping
                            gremlinThreadAddress=gremlinThreadAddress)
