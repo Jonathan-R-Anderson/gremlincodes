22ac3eb1fd427f3410abfff2854f258cf2bef73b
Jonathan Anderson <jranderson404@gmail.com>
2024-09-17 12:03:45 -0400

    new index

--- shared.py
+++ shared.py
@@ -35,7 +35,7 @@ gremlinThreadAddress = '0x7aA9305b453Cd5Ad1C6dDcaEbb14Af9febB83199'
 gremlinAdminAddress = '0x69B0C4FDAC564C8DC5Eb64e9cAFe691f4af6BF94'
 
 # Contract ABIs
-gremlinThreadABI = [json.dumps(
+gremlinThreadABI = [
     {
       "inputs": [],
       "stateMutability": "nonpayable",
@@ -281,10 +281,10 @@ gremlinThreadABI = [json.dumps(
       "outputs": [],
       "stateMutability": "nonpayable",
       "type": "function"
-    })
+    }
 ]
-
-gremlinAdminABI = [json.dumps(
+gremlinThreadABI = [json.dumps(item) for item in gremlinThreadABI]
+gremlinAdminABI = [
     {
       "inputs": [
         {
@@ -349,8 +349,9 @@ gremlinAdminABI = [json.dumps(
       "outputs": [],
       "stateMutability": "nonpayable",
       "type": "function"
-    })
+    }
 ]
+gremlinAdminABI = [json.dumps(item) for item in gremlinAdminABI]
 
 # Contract objects
 #gremlinThreadContract = web3.eth.contract(address=gremlinThreadAddress, abi=gremlinThreadABI)
