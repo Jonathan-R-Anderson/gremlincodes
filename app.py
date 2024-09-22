4873d932749c0ac0460bc03353a82b8bc21be561
Jonathan Anderson <jranderson404@gmail.com>
2024-09-22 09:57:05 -0400

    Adding ssl

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ import json
 import urllib
 from shared import app, FILE_DIR, TORRENT_DIR, TRACKER_PORT, auto_seed_static_files
 from blueprints.routes import blueprint
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_cors import CORS
 
+CORS(app)
 
 app.register_blueprint(blueprint)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
 # Setup logging
 logging.basicConfig(level=logging.DEBUG)
